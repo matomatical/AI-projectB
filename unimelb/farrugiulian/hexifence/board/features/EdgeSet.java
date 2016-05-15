3e5ea25d363e2c2a7ff39e62f116f934a817d1f2
Matt F <matomatical@gmail.com>
2016-05-15 19:13:29 +1000

    update in edgeset now complete

--- unimelb/farrugiulian/hexifence/board/features/EdgeSet.java
+++ unimelb/farrugiulian/hexifence/board/features/EdgeSet.java
@@ -36,67 +36,104 @@ public class EdgeSet {
 		sacrificing = new QueueHashSet<Edge>();
 	}
 	
+	/** Update the feature set in response to the placing of an edge
+	 * @param edge The edge that has been placed
+	 **/
 	public void update(Edge edge) {
 		
 		// remove this edge from any set it was in, it is no longer empty
-		safe.remove(edge);
-		free.remove(edge);
-		capturing.remove(edge);
-		sacrificing.remove(edge);
 		
-		// track all potentially-affected edges
+		remove(edge);
+		
+		// track all potentially-affected edges, ensuring they are in the
+		// right collection
+		
 		for(Cell cell : edge.getCells()){
-			
-			int n = cell.numEmptyEdges();
-			
-			if(n == 2){
-				// these edges are no longer safe!
-				for(Edge e : cell.getEmptyEdges()){
-					safe.remove(e);
-				}
-			
-			} else if (n == 1){
-				// these edges are no longer sacrifices, they're free!
-				for(Edge e : cell.getEmptyEdges()){
-					capturing.add(e);
-				}
+			for(Edge neighbour : cell.getEmptyEdges()){
+				reclassify(neighbour);
 			}
 		}
 	}
 	
+	/** Update the feature set in response to the un-placing of an edge
+	 * @param edge The edge that has been un-placed
+	 **/
 	public void rewind(Edge edge) {
-		
-		// this piece has been removed from the board,
-		// we better update our collections!
 
-		// but wait! this could affect other nearby edges too!
+		// re-add the edge and reclassify potentially-affected edges, ensuring
+		// they are in the right collection
 		
 		for(Cell cell : edge.getCells()){
-			
-			int n = cell.numEmptyEdges();
-			
-			if(n == 1){
-				// this was the only edge, this cell's a freebie now
-				capturing.add(edge);
-				
-			} else if(n == 2){
-				// these edges are no longer free! they're
-				// sacrifices now
-				for(Edge e : cell.getEmptyEdges()){
-					capturing.remove(e);
-				}
-				
-			} else if (n > 2){
-				// these edges are now no longer sacrifices, they're all safe! TODO 79 characters
-				for(Edge e : cell.getEmptyEdges()){
-					if (e.getOtherCell(cell) == null || e.getOtherCell(cell) != null && e.getOtherCell(cell).numEmptyEdges() > 2) {
-						safe.add(e);
-					}
-				}
+			for(Edge e : cell.getEmptyEdges()){
+				reclassify(e);	
 			}
 		}
 	}
 
+	private void remove(Edge edge){
+		safe.remove(edge);
+		free.remove(edge);
+		capturing.remove(edge);
+		sacrificing.remove(edge);	
+	}
+	
+	private void reclassify(Edge edge){
+		
+		// remove this edge from any set it was in, so that we can find the
+		// right place for it
+		this.remove(edge);
+		
+		if (! edge.isEmpty()){
+			// this edge is no longer free! don't put it in any collection
+			return;
+			
+		} else if (edge.numCapturableCells() == 0){
+			// decide between safe and sacrificing
+			
+			for(Cell cell : edge.getCells()){
+				if(cell.numEmptyEdges() == 2){
+					// this edge is NOT safe, it's sacrificing!
+					sacrificing.add(edge);
+					return;
+				}
+			}
+			
+			// if we make it through that loop, it's not sacrificial on
+			// either side, we can add it to the safe set!
+			safe.add(edge);
+		
+		} else {
+			// decide between capturing and free
+			
+			// is it a double freebie?
+			if(edge.numCapturableCells() == 2){
+				// definitely free!
+				free.add(edge);
+				return;
+			}
+			
+			// otherwise, it could be either, let's check out 
+			
+			for(Cell cell : edge.getCells()){
+				if(cell.numEmptyEdges() == 1){
+					// this is the cell we are looking for
+					
+					Cell other = edge.getOtherCell(cell);
+					
+					if(other != null && other.numEmptyEdges() == 2){
+						// part of a chain, this capture has consequences!
+						capturing.add(edge);
+					} else {
+						// side of the baord or not part of the chain, freee!
+						free.add(edge);
+					}
+					
+					// either way, we're done here
+					return;
+				}
+			}
+		}
+	}
 	
 	
 	
